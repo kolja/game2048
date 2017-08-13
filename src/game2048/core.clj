93937a6854c3c8af295abd7872e47d03b7bc1d8b
Kolja Wilcke <kolja.wilcke@zalando.de>
2017-08-13 14:53:48 +0200

    inline silde function

--- src/game2048/core.clj
+++ src/game2048/core.clj
@@ -20,23 +20,17 @@
                  (apply str (map #(format "%5d " %) row)))]
     (println line "\n")) v)
 
-(defn slide [board]
- (map (comp
-        #(take 4 (concat % [0 0 0 0]))
-        (fn [v] (r (reduce #(let [l (last %1)]
-                              (if (= %2 l)
-                                (conj (pop %1) (+ l %2) 0)
-                                (conj %1 %2)))
-                           [] v)))
-        r
-        ) board))
-
 (defn -main []
   (loop [board (insert-2 (partition 4 (repeat 16 0)))]
     (when (render board)
       (let [input ({"h" 4 "k" 3 "l" 2 "j" 1} (read-line))
             command (apply comp
                            (assoc (into [] (repeat 5 (fn [v] (map (fn [n] (map #(nth % n) v)) [3 2 1 0]))))
-                                  input slide))]
+                                  input (partial map (comp #(take 4 (concat % [0 0 0 0]))
+                                                           (fn [v] (r (reduce #(let [l (last %1)]
+                                                                                 (if (= %2 l)
+                                                                                   (conj (pop %1) (+ l %2) 0)
+                                                                                   (conj %1 %2)))
+                                                                              [] v))) r))))]
         (-> board command insert-2 recur)))))
 
